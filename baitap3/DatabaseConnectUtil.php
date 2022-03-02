bdee61db855089f8ed45a65ee7a8ecc0cda819c6
levanlong2911 <vanlong11x1@gmail.com>
2022-03-02 14:36:11 +0700

    save5

--- baitap3/DatabaseConnectUtil.php
+++ baitap3/DatabaseConnectUtil.php
@@ -9,7 +9,7 @@
     
     // Port: 3306
     
-    $localhost = "remotemysql.com";
+    $localhost = "https://remotemysql.com/";
     $username = "rMEt9axaDn";
     $password = "70xIYdFRXw";
     $database = "rMEt9axaDn";
