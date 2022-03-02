96d1b9053137c9a19585f0b3daf69bbb80801a31
levanlong2911 <vanlong11x1@gmail.com>
2022-03-02 10:18:20 +0700

    add file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài tập</title>
+</head>
+<body>
+    <h1>Bài tập</h1>
+    <h2>bài tập 1</h2>
+    <a href="/baitap1/bai1.php">Bài 1</a>
+</body>
+</html>
