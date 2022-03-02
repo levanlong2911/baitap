716f1c1603f46555f556f3dfcd897dfbdab1c6f4
levanlong2911 <vanlong11x1@gmail.com>
2022-03-02 13:48:57 +0700

    save connect

--- baitap1/bai10.php
+++ baitap1/bai10.php
@@ -2,6 +2,7 @@
 $array = array('0', '1', '2', '3', '4');
 echo 'Độ dài của mảng là: ' . count($array) . '<br />';
 print_r($array);
+echo '<br/>';
 array_shift($array);
 print_r($array);
 unset($array[3]);
--- baitap3/DatabaseConnectUtil.php
+++ baitap3/DatabaseConnectUtil.php
@@ -1,8 +1,17 @@
 <?php
+    // Username: rMEt9axaDn
+
+    // Database name: rMEt9axaDn
+    
+    // Password: 70xIYdFRXw
+    
+    // Server: remotemysql.com
+    
+    // Port: 3306
     $localhost = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'login';
+    $username = 'rMEt9axaDn';
+    $password = '70xIYdFRXw';
+    $database = 'rMEt9axaDn';
 
     $mysqli = new mysqli($localhost, $username, $password, $database);
     $mysqli -> set_charset('utf8');
