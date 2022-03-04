c8fb54e6686252e6a5993327310ea6dfa1a6d990
levanlong2911 <vanlong11x1@gmail.com>
2022-03-04 20:00:14 +0700

    index

--- baitap4/signup.php
+++ baitap4/signup.php
@@ -107,7 +107,7 @@
                                 
                         
                                 try {
-                                    $mail->SMTPDebug = 0; //0,1,2: chế độ debug. khi chạy ngon thì chỉnh lại 0 nhé
+                                    $mail->SMTPDebug = 2; //0,1,2: chế độ debug. khi chạy ngon thì chỉnh lại 0 nhé
                                     $mail->isSMTP();  
                                     $mail->CharSet  = "utf-8";
                                     $mail->Host = 'smtp.gmail.com';  //SMTP servers (địa chỉ email server)
